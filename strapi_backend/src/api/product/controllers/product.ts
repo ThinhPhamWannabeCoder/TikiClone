03d8eaacd3e698d1d2fcc0f99a84308a4bba85e4
thinhpham4903 <thinhpham4903@gmail.com>
2024-03-27 11:54:20 +0700

    add first flexible method getAllHome

--- strapi_backend/src/api/product/controllers/product.ts
+++ strapi_backend/src/api/product/controllers/product.ts
@@ -7,29 +7,39 @@ import { factories } from '@strapi/strapi'
 interface allProduct{
     category_id?: number,
     subcategory_id?: number,
-    price_range?: string | string[],
+    price_range?: string ,
     best_seller?: boolean,
     new_product?: boolean,
-    sort?: string | string[],
+    sort?: string,
+    limit?: number,
+    current_page?: number
 }
 export default factories.createCoreController('api::product.product',({strapi})=>({
     async getAll(ctx, next){
+        // Neu nhu variable khong ung thi tra ve error trong method luon, vi neu de day se rat dai va kho doc
+        // Lam the nao de valuate no ok hon 1 chut
+
         // Phai
         // All will be string -> convert to id
-        const {category_id, subcategory_id, price_range, best_seller,new_product, sort} = ctx.request.query;
+        const {category_id, subcategory_id, price_range, best_seller,new_product, sort, limit, current_page} = ctx.request.query;
         const queryParams: allProduct = {};
 
 // Check if query parameter exists before assigning it to interface property
-        if (category_id) queryParams.category_id = parseInt(category_id[0]);
-        if (subcategory_id) queryParams.subcategory_id = parseInt(subcategory_id[0]);
-        if (price_range) queryParams.price_range = price_range;
-        if (best_seller) queryParams.best_seller = best_seller === "true";
-        if (new_product) queryParams.new_product = new_product === "true";
-        if (sort) queryParams.sort = sort;
-
-        const data = await strapi.service('api::product.product').getAll(queryParams);       
+        if (category_id) queryParams.category_id = parseInt(category_id as string);
+        if (subcategory_id) queryParams.subcategory_id = parseInt(subcategory_id as string);
+        if (price_range) queryParams.price_range = price_range as string;
+        if (best_seller) queryParams.best_seller = best_seller as string === "true" ;
+        if (new_product) queryParams.new_product = new_product as string === "true" ;
+        if (sort) queryParams.sort = sort as string;
+        if (limit) queryParams.limit = parseInt(limit as string);
+        if (current_page) queryParams.current_page = parseInt(current_page as string);
+        //const data = await strapi.service('api::product.product').getAll(queryParams);   
+            
+        // if()
+        const result = await strapi.service('api::product.product').getAllHome({best_seller: queryParams.best_seller, limit: queryParams.limit, current_page: queryParams.current_page});
         
-        ctx.body = data;
+        ctx.body = result;
+        
     },
     async bestSeller(ctx, next){
         // Lam sao de lay va handle tung du lieu trong nay
--- strapi_backend/src/api/product/services/product.ts
+++ strapi_backend/src/api/product/services/product.ts
@@ -17,15 +17,15 @@ interface result{
 interface allProduct{
     category_id?: number,
     subcategory_id?: number,
-    price_range?: string | string[],
+    price_range?: string,
     best_seller?: boolean,
     new_product?: boolean,
-    sort?: string | string[],
+    sort?: string,
     limit?: number,
-    currentPage?: number
+    current_page?: number
 }
 import { factories } from '@strapi/strapi';
-
+//@ts-ignore
 export default factories.createCoreService('api::product.product',({strapi})=>({
     getAll: async (options: allProduct) =>{
        //Handle tai day
@@ -33,13 +33,73 @@ export default factories.createCoreService('api::product.product',({strapi})=>({
         // 
         return(options)
     },
-    getAllHome: async (best_seller, limit, current_page)=>{
-        // Phan cuoi
+    getAllHome: async (options: {best_seller: boolean, limit:number, current_page:number})=>{
+        // All the variable Have to be avaialble
+        // Limit = 10
+        // Current-Page = 1
+        // Best Seller: true, false
+        // Lam sao de lay duoc thong tin 
+        if(options.best_seller){
+            // console.log(typeof options.best_seller)
 
-        return 1;
+            return await strapi.entityService.findMany('api::product.product',{
+                fields: ['id', 'name', 'price','Inventory'],
+                populate:{
+                    product_sub_category: {
+                        fields: ['id', 'name'],
+                        populate:{
+                            product_category:{
+                                fields: ['id', 'name']
+                            }
+                        }
+                    },
+                    primary_image:{
+                        fields: ['id','url']
+                    }
+                },
+                start: (options.current_page - 1)*options.limit,
+                limit: options.limit,
+                sort: [{price: 'desc'},{Inventory: 'desc'}],
+                filters: {
+                    price: {
+                        $gte: 0
+                    },
+                    Inventory: {
+                        $gte: 0
+                    }
+                }
+            })
+        }
+        return await strapi.entityService.findMany('api::product.product',{
+            fields: ['id', 'name', 'price','Inventory'],
+            populate:{
+                product_sub_category: {
+                    fields: ['id', 'name'],
+                    populate:{
+                        product_category:{
+                            fields: ['id', 'name']
+                        }
+                    }
+                },
+                primary_image:{
+                    fields: ['id','url']
+                }
+            },
+            start: (options.current_page - 1)*options.limit,
+            limit: options.limit,
+            // sort: [{price: 'desc'},{Inventory: 'desc'}],
+            filters: {
+                price: {
+                    $gte: 0
+                },
+                Inventory: {
+                    $gt: 0
+                }
+            }
+        })
     },
     getAllCategory: async (category_id, best_seller, price_range,limit, current_page) => {
-
+        // tuong tu va co rat nhieu
     },
     getBestByCategory: async (category_id, limit, current_page) =>{
 
@@ -51,6 +111,6 @@ export default factories.createCoreService('api::product.product',({strapi})=>({
 
     },
     getProductById: async (product_id: number) =>{
-
+        // Process all Information -> Later
     }
 }));
