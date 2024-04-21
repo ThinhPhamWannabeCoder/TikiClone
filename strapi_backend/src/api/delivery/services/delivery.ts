c1779ee038a1080a20dbf9cd9defd3fe81901936
thinhpham4903 <thinhpham4903@gmail.com>
2024-04-21 17:47:50 +0700

    Finish Delivery GET API for customer to get delivery options

--- strapi_backend/src/api/delivery/services/delivery.ts
+++ strapi_backend/src/api/delivery/services/delivery.ts
@@ -7,35 +7,35 @@ import { factories } from '@strapi/strapi';
 export default factories.createCoreService('api::delivery.delivery',({strapi})=>({
     getAll:async ()=>{
         const data = await strapi.entityService.findMany('api::delivery.delivery',{
-            populate: '*',
+            fields: ["name","description","base_price","duration"]
           
         })
-        const res = data.map(item=> {
-            return {
-                id: item.id,
-                name: item.name,
-                description: item.description,
-                base_price: item.base_price
-            }
-        })
-        return res
+        // const res = data.map(item=> {
+        //     return {
+        //         id: item.id,
+        //         name: item.name,
+        //         description: item.description,
+        //         base_price: item.base_price,
+        //         duration: item.duration
+        //     }
+        // })
+        return data
     },
     getById:async (id:number)=>{
-        const data = await strapi.entityService.findMany('api::delivery.delivery',{
-                populate: '*',
-                fitlers:{
-                    id: id                 
-                }
+        const data = await strapi.entityService.findOne('api::delivery.delivery',id, {
+                fields: ["name","description","base_price","duration"]
             })
-        const res = data.map(item=> {
-            return {
-                id: item.id,
-                name: item.name,
-                description: item.description,
-                base_price: item.base_price
-            }
-        })
-        return res
+        // const res = data.map(item=> {
+        //     return {
+        //         id: item.id,
+        //         name: item.name,
+        //         description: item.description,
+        //         base_price: item.base_price,
+        //         duration: item.duration
+
+        //     }
+        // })
+        return data
 
     },
     createNew: async()=>{
