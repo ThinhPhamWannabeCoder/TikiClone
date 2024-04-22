4ea859b8ec97069dcb9879b66c554bf39f95a484
thinhpham4903 <thinhpham4903@gmail.com>
2024-04-22 14:30:50 +0700

    ISSUE: ORDERING: STRAPI DOESN'T ALLOW ME TO DO BULK INSERT ON REALTION
    Soluction 1: discard relation
    Solution 2: Call API for each order
    Solution 3: find a way to insert in forLoops

--- strapi_backend/src/api/order/services/order.ts
+++ strapi_backend/src/api/order/services/order.ts
@@ -3,24 +3,82 @@
  */
 
 import { factories } from '@strapi/strapi';
+import product from '../../product/controllers/product';
 
 export default factories.createCoreService('api::order.order',({strapi})=>({
     createNewOrder:async (body)=>{
         // TODO PROCESSING
         const {userId, addressId, deliveryId, orders} = body
+        const orderData = [];
+        const orderDetailData = []
+        const productsData = []
         // console.log(products)
         orders.forEach(item=>{
-            console.log(item)
+            let totalCost = 0 ;
+            let productData  = []
+            item.products.forEach(product=>{
+                productData.push({
+                    id: product.id,
+                    quantity: product.quantity,
+                    totalPrice: product.quantity  * product.price
+                })
+                totalCost += product.quantity  * product.price
+            })
+            productsData.push({
+                products: productData
+            })
+            const data = {
+                user: userId,
+                address: addressId,
+                store: item.storeId,
+                delivery: deliveryId,
+                deliveryTotalCost: item.deliveryCost,
+                productTotalCost: totalCost,
+            }    
+            orderData.push(data)
+            // console.log(data)
+        })
             // CREATE ORDER
             // const id = await strapi.entittySerivce('api::order.order').create({},{})
             // CALCULATE AGGREATED VALUE
             // CREATE ORDER_DETAIL
+            let {ids} = await strapi.db.query('api::order.order').createMany({
+                data: [
+                    {
+                      user: 1,
+                      address: 1,
+                      store: 1,
+                      delivery: 1,
+                      deliveryTotalCost: 50000,
+                      productTotalCost: 12000
+                    },
+                    {
+                      user: 1,
+                      address: 1,
+                      store: 2,
+                      delivery: 1,
+                      deliveryTotalCost: 25000,
+                      productTotalCost: 10000
+                    }
+                   ]
+                   
+            });
 
-        })
-        // console.log(groupedData)
-        // return groupedData;
-        // test
-        return  "Tested"
+            ids.forEach((value, index)=> {
+                orderDetailData.push({
+                    order: value,
+                    product: 3,
+                    quantity: 20,
+                    totalPrice: 1000,
+                })
+            })
+            console.log(orderDetailData)
+
+            const result  = await strapi.db.query('api::oder-detail.oder-detail').createMany({
+                data: orderDetailData
+            });
+       
+        return  result
 
     },
     // getAllOrder:async ()=>{
@@ -36,3 +94,22 @@ export default factories.createCoreService('api::order.order',({strapi})=>({
 
     // }
 }));
+
+[
+ {
+   user: 1,
+   address: 1,
+   store: 1,
+   delivery: 1,
+   deliveryTotalCost: 50000,
+   productTotalCost: 12000
+ },
+ {
+   user: 1,
+   address: 1,
+   store: 2,
+   delivery: 1,
+   deliveryTotalCost: 25000,
+   productTotalCost: 10000
+ }
+]
